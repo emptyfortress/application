d1416d29afbe49fdcadcd626816281021f56b6b7
Volkov Alexander <volkov.525@gmail.com>
2024-05-23 14:40:57 +0300

    tree in version

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -49,29 +49,33 @@ const columns = [
 const rows = ref([
 	{
 		id: 0,
-		name: 'Квартальные цели',
-		version: '0.1.0',
+		name: 'Служебная записка',
+		version: '0.1.5',
 		descr: 'Здесь описание приложения',
 		author: 'Орлов П.С.',
-		created: '2023-10-14',
-	},
-	{
-		id: 1,
-		name: 'Тестовое приложение',
-		version: '1.1.0',
-		descr: 'Здесь описание приложения',
-		author: 'Орлов П.С.',
-		created: '2023-10-12',
-	},
-	{
-		id: 2,
-		name: 'Заявка на отпуск',
-		version: '2.1.0',
-		descr: 'Здесь описание приложения',
-		author: 'Воробьева А.К.',
 		created: '2023-10-14',
 	},
 ])
+
+const tree = [
+	{
+		label: '0.1.5',
+		children: [
+			{
+				label: '0.1.4',
+			},
+			{
+				label: '0.1.3',
+			},
+		],
+	},
+	{
+		label: '0.0.1',
+	},
+	{
+		label: 'alfa',
+	},
+]
 
 const goto = (evt: Event, row: any, index: number) => {
 	router.push(`/app/${row.name}`)
@@ -85,32 +89,48 @@ const action = () => {
 const create = (e: any) => {
 	rows.value.push(e)
 }
+const selected = ref('0.1.5')
 </script>
 
 <template lang="pug">
 q-page(padding)
 	h4 Мои приложения ({{rows.length}})
-	q-table(flat
+	q-table.q-mt-sm(flat
 		:rows="rows"
 		:columns="columns"
 		row-key="name"
 		@row-click="goto"
 		hide-bottom)
 
+		template(v-slot:body-cell-version="props")
+			q-td(:props="props")
+				q-badge(color="positive" @click.stop="") {{props.value}}
+					q-menu
+						q-tree(:nodes="tree"
+							v-model:selected="selected"
+							dense
+							node-key="label"
+							default-expand-all)
+
 	.q-gutter-x-lg
 		q-btn.q-mt-sm(flat icon="mdi-plus-circle" color="primary" label="Создать" @click="action") 
-		q-btn.q-mt-sm(flat icon="mdi-arrow-right-thick" color="primary" label="Перенести" @click="") 
 
 	CreateDialog(v-model="dialog" @create="create")
 </template>
 
 <style scoped lang="scss">
-.zg {
-	font-size: 1.3rem;
-	font-weight: 300;
-}
 :deep(.q-table th) {
 	background: $primary;
+	color: white;
+}
+.q-tree {
+	margin: 1rem;
+}
+:deep(.q-tree__node--selected) {
+	background: $secondary;
+	color: white;
+}
+:deep(.q-tree__node--selected .q-tree__node-header-content) {
 	color: white;
 }
 </style>
