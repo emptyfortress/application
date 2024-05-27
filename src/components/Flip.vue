c0771c2582c85a26566440749cf3d11ea943f38e
Volkov Alexander <volkov.525@gmail.com>
2024-05-27 16:39:00 +0300

    add fade to buttons

--- src/components/Flip.vue
+++ src/components/Flip.vue
@@ -12,11 +12,17 @@ const full = ref(false)
 
 const active = ref(0)
 
+const emit = defineEmits(['active', 'inactive'])
+
 const expand = (e: number) => {
 	full.value = !full.value
 	if (active.value == e) {
 		active.value = 0
-	} else active.value = e
+		emit('inactive')
+	} else {
+		active.value = e
+		emit('active')
+	}
 }
 const grid = ref()
 
@@ -45,17 +51,17 @@ onMounted(() => {
 <template lang="pug">
 .grido(ref="grid" :class="{full : full}")
 	.item(@click="expand(1)" :class="{active : active == 1}")
-		.rel
-			.txt fuck you
+		.in
+			.txt Роли
 	.item(@click="expand(2)" :class="{active : active == 2}")
-		div
-			ExpandCard
+		.in
+			.txt  Процессы
 	.item(@click="expand(3)" :class="{active : active == 3}")
-		div
-			ExpandCard
+		.in
+			.txt Карточки
 	.item(@click="expand(4)" :class="{active : active == 4}")
-		div
-			ExpandCard
+		.in
+			.txt Реестры
 </template>
 
 <style scoped lang="scss">
@@ -64,31 +70,28 @@ onMounted(() => {
 	grid-template-columns: 1fr 1fr 1fr 1fr;
 	grid-template-rows: 200px 100px;
 	gap: 1rem;
-	// grid-gap: 1rem;
-	// height: 800px;
 	.item {
 		cursor: pointer;
 		background-color: white;
 		padding: 1rem;
-		height: 100%;
-		// &:hover {
-		// 	border: 1px solid #ccc;
-		// 	box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2);
-		// }
+		&:hover {
+			border: 1px solid #ccc;
+			box-shadow: 2px 2px 6px rgba(0, 0, 0, 0.2);
+		}
 	}
 	&.full {
 		grid-template-rows: 1fr 100px;
 		.active {
 			grid-column: 1/-1;
 			grid-row: 1/2;
-			height: calc(100vh - 300px);
+			height: calc(100vh - 350px);
 		}
 	}
 }
 
-// .txt {
-// 	position: absolute;
-// 	top: 1rem;
-// 	left: 1rem;
-// }
+.txt {
+	text-align: center;
+	text-transform: uppercase;
+	color: $secondary;
+}
 </style>
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -23,42 +23,49 @@ const store = useStore()
 store.setPage(props.id)
 
 const version = ref('0.1.0')
+
+const active = ref(false)
 </script>
 
 <template lang="pug">
 q-page(padding)
 	.grid
 		div
-			h4 {{ props.id}}
-			.text-subtitle1 Простые служебные записки
-		.bl
-			.text-overline version
-			.big
-				q-icon(name="mdi-source-branch" color="primary")
-				span.q-ml-sm v. 0.1.0
-					q-menu
-						q-list
-							q-item(clickable)
-								q-item-section v.0.2.0
-							q-item(clickable)
-								q-item-section v.1.1.0
+			h4(:class="{expand: active}") {{ props.id}}
+			.text-subtitle1(v-if="!active") Простые служебные записки
+		transition(name="fade")
+			.bl(v-if="!active")
+				.text-overline version
+				.big
+					q-icon(name="mdi-source-branch" color="primary")
+					span.q-ml-sm v. 0.1.0
+						q-menu
+							q-list
+								q-item(clickable)
+									q-item-section v.0.2.0
+								q-item(clickable)
+									q-item-section v.1.1.0
 
-		.q-gutter-x-sm
-			q-btn(flat  round icon="mdi-pencil" color="primary" @click="") 
-				q-tooltip Редактировать
-			q-btn(flat  round icon="mdi-content-duplicate" color="primary" @click="") 
-				q-tooltip Дублировать
-			q-btn(unelevated color="negative" label="Удалить приложение" @click="") 
+		transition(name="fade")
+			.q-gutter-x-sm(v-if="!active")
+				q-btn(flat  round icon="mdi-pencil" color="primary" @click="") 
+					q-tooltip Редактировать
+				q-btn(flat  round icon="mdi-content-duplicate" color="primary" @click="") 
+					q-tooltip Дублировать
+				q-btn(unelevated color="negative" label="Удалить приложение" @click="") 
 
-	.podzag Настройки приложения
-	Flip
+	.podzag(:class="{expand: active}") Настройки приложения
+	Flip(@active="active = true" @inactive="active = false")
 
 </template>
 
 <style scoped lang="scss">
-.txt {
-	text-transform: uppercase;
-	color: $secondary;
+h4 {
+	transition: 0.2s ease all;
+	&.expand {
+		font-size: 1.2rem;
+		font-weight: 600;
+	}
 }
 .grid {
 	display: grid;
@@ -83,6 +90,5 @@ q-page(padding)
 	&.expand {
 		margin-top: 0;
 	}
-	// font-weight: 300;
 }
 </style>
