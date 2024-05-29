d5523e8b5f4fcf77e666363ce5dcd7bf8d248faa
Volkov Alexander <volkov.525@gmail.com>
2024-05-29 15:49:17 +0300

    WIP

--- src/components/PropertyPanel.vue
+++ src/components/PropertyPanel.vue
@@ -10,7 +10,7 @@ const router = useRouter()
 const route = useRoute()
 
 const goto = () => {
-	store.setPage(route.params.id)
+	store.setPage(route.params.id.toString())
 	router.push('/subcard')
 }
 </script>
