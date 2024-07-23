de52ce1cef4445d7628d1f5fb34a9795776235c3
Volkov Alexander <volkov.525@gmail.com>
2024-07-23 13:33:32 +0300

    add list page

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
 import { useStore } from '@/stores/store'
 const store = useStore()
+
+const list = [
+	{ id: 0, label: 'Все карточки', descr: 'все доступные карточки' },
+	{ id: 1, label: 'Список 1', descr: 'здесь описание списка' },
+	{ id: 2, label: 'Список 2', descr: 'здесь описание списка' },
+	{ id: 3, label: 'Список 3', descr: 'здесь описание списка' },
+	{ id: 4, label: 'Список 4', descr: 'здесь описание списка' },
+]
 </script>
 
 <template lang="pug">
-.bl.center
-	div Тут списки
+.bl
+	.q-mb-md Списки состоят из поисковых запросов и представлений.
+	q-list
+		q-expansion-item(label='Список 1' icon='mdi-script-text-outline' v-for="item in list" :key='item.id')
+			template(v-slot:header)
+				q-item-section(avatar)
+					q-icon(name='mdi-script-text-outline')
+				q-item-section()
+					q-item-label {{ item.label }}
+				q-item-section()
+					q-item-label {{ item.descr }}
+				q-item-section(side)
+					.row.q-gutter-x-sm
+						q-btn(flat round icon="mdi-content-duplicate" color="primary" dense size='sm') 
+						q-btn(flat round icon="mdi-trash-can-outline" color="primary" dense size='sm') 
+	br
+	q-btn(unelevated color="primary" label="Создать список") 
 			
 </template>
 
