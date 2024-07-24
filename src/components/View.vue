d1feaf2ee30a44a7067061e5e6adb13bddf4b7a6
Volkov Alexander <volkov.525@gmail.com>
2024-07-24 14:55:08 +0300

    add grid table

--- src/components/View.vue
+++ src/components/View.vue
@@ -1,8 +1,99 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import draggable from 'vuedraggable'
+
+const list = ref<ColNode[]>([
+	{
+		id: 0,
+		text: 'fuck',
+	},
+])
+
+const remove = (el: any) => {
+	list.value.splice(el, 1)
+}
+</script>
 
 <template lang="pug">
 .bl
-	h2 Hello
+	.zagg Настройка представления
+	draggable(:list='list'
+		item-key="id"
+		tag="ul"
+		class="list-group"
+		ghost-class="ghost"
+		:ondragover="end"
+		:ondrop="insert"
+		:group="{ name: 'data', pull: 'clone', put: false }"
+		)
+
+		template(#item="{ element, index }")
+			li.list-group-item {{ element.text }}
+				q-btn.close(flat round icon="mdi-close" @click="remove(index)" size="xs" dense) 
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.zagg {
+	font-size: 1.4rem;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+ul:empty:after {
+	display: block;
+	content: 'Пусто. Настройте колонки, перетащив их из дерева справа.';
+	width: 100%;
+	text-align: center;
+	color: rgba(0, 0, 0, 0.5);
+	height: 75px;
+	line-height: 75px;
+	border-radius: 4px;
+	font-size: 0.9rem;
+}
+.list-group {
+	// box-sizing: border-box;
+	display: grid;
+	grid-template-columns: repeat(v-bind(col), 1fr);
+	border: 1px solid #e0e0e0;
+	// border-bottom: none;
+	// background: rgba(0, 0, 0, 0.07);
+	background: white;
+	padding: 1px;
+	gap: 2px;
+}
+.list-group-item {
+	// font-size: v-bind(headsize);
+	// background: v-bind('view.head.bgColor');
+	cursor: move;
+	// color: v-bind('view.head.fontColor');
+	padding: 0.7rem 1rem;
+	line-height: 1.2;
+	position: relative;
+	// text-align: v-bind('view.head.align');
+	// font-weight: v-bind('view.head.weight');
+	// font-style: v-bind('view.head.style');
+	border: 1px solid #e0e0e0;
+	min-width: 127px;
+	.close {
+		position: absolute;
+		top: 1px;
+		right: 1px;
+		visibility: hidden;
+	}
+	&:hover .close {
+		visibility: visible;
+	}
+}
+ul,
+li {
+	margin: 0;
+	padding: 0;
+	list-style: none;
+}
+.ghost {
+	padding: 0.5rem 1rem;
+	// height: 75px;
+	opacity: 0.5;
+	background: #ccc;
+}
+</style>
