611380ab0b1a1f60b8270ee1b5bfbb25cd242a96
Volkov Alexander <volkov.525@gmail.com>
2024-09-26 11:40:30 +0300

    add step 2

---
+++ src/components/Step2.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { uid } from 'quasar'
+
+const rolename = ref('')
+
+const roles = ref([{ id: 'ini', name: 'Инициатор (встроенная)', trash: false }])
+const add = () => {
+	if (rolename.value.length > 0) {
+		roles.value.push({
+			id: uid(),
+			name: rolename.value,
+			trash: true,
+		})
+		rolename.value = ''
+	}
+}
+const destroy = (e: number) => {
+	roles.value.splice(e, 1)
+}
+</script>
+
+<template lang="pug">
+q-form(@submit='add')
+	div Кто участвует в процессе? Список должен включать как непосредственных участников, так и "наблюдателей".
+	q-list.q-mb-md
+		q-item(clickable v-for="(role, index) in roles" :key="role.id")
+			q-item-section(avatar)
+				q-icon(name="mdi-account" color="primary")
+			q-item-section {{ role.name }}
+			q-item-section(side v-if='role.trash')
+				q-btn(flat round icon="mdi-trash-can-outline" color="primary" @click='destroy(index)') 
+
+	.row.items-center
+		q-input(v-model="rolename" label='Участник процесса' dense outlined bg-color="white")
+		q-btn(flat color="primary" label="Добавить" type='submit') 
+		// q-btn(flat color="primary" label="Добавить" @click="add(rolename)" type='submit') 
+</template>
+
+<style scoped lang="scss">
+.q-list {
+	margin-bottom: 1rem;
+	max-width: 800px;
+}
+.q-input {
+	width: 300px;
+	margin-bottom: 3px;
+}
+</style>
--- src/views/Assistent.vue
+++ src/views/Assistent.vue
@@ -1,17 +1,62 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import Step2 from '@/components/Step2.vue'
+
+const step = ref(1)
+</script>
 
 <template lang="pug">
 q-page
-	.cont
-		h5 Здесь будет ассиcтент
+	q-stepper(
+		flat
+		v-model="step"
+		vertical
+		color="primary"
+		header-nav
+		animated)
+
+		q-step(
+			:name="1"
+			title="Заполните атрибуты"
+			prefix="1"
+			:done="step > 1")
+
+			div Сначала вам необходимо заполнить следующие атрибуты:
+			q-input(v-model="author" label='Автор' dense outlined bg-color="white")
+			q-input(v-model="start" label='Дата начала' dense outlined bg-color="white")
+			q-input(v-model="end" label='Дата окончания' dense outlined bg-color="white")
+			q-input(v-model="button" label='Название кнопки старта' dense outlined bg-color="white")
+
+			q-stepper-navigation
+				q-btn(@click="step = 2" color="primary" label="Далее")
+
+		q-step(
+			:name="2"
+			title="Определите участников процесса (роли)"
+			prefix="2"
+			:done="step > 2")
+
+			Step2
+
+		q-step(
+			:name="3"
+			title="Определите участников процесса (роли)"
+			prefix="2"
+			:done="step > 2")
+			
 </template>
 
 <style scoped lang="scss">
-.cont {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	height: 75vh;
-	width: 100%;
+.q-stepper {
+	background: initial;
+	// width: 1200px;
+}
+// .q-list {
+// 	margin-bottom: 1rem;
+// 	max-width: 800px;
+// }
+.q-input {
+	width: 300px;
+	margin-bottom: 3px;
 }
 </style>
