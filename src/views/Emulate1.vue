4235479a7b643c5192c6b505a10d9ff59c30a319
Volkov Alexander <volkov.525@gmail.com>
2024-10-17 15:13:58 +0300

    fix minimap

--- src/views/Emulate1.vue
+++ src/views/Emulate1.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import { ref, reactive } from 'vue'
-import { computed } from 'vue'
+import { ref, onMounted } from 'vue'
 import VueDraggableResizable from 'vue-draggable-resizable'
 import FormTopEmulate from '@/components/FormTopEmulate.vue'
 import { useRouter, useRoute } from 'vue-router'
 import { useForms } from '@/stores/forms'
 import FormEmulate1 from '@/components/FormEmulate1.vue'
-import FormEmulate2 from '@/components/FormEmulate2.vue'
+import { gsap } from 'gsap'
 
-const router = useRouter()
 const route = useRoute()
 const myform = useForms()
 
@@ -23,29 +21,18 @@ const next = (e: string, r: string) => {
 	options.value = [r, 'Все остальные']
 }
 
-const calcImg = computed(() => {
-	switch (route.params.page) {
-		case '1': // Согласовать
-			return '/nav2.png'
-		case '2': // Исправить заявку
-			return '/nav3.png'
-		case '3': // Рассмотреть заявку
-			return '/nav4.png'
-		case '4': // Исполнить заявку
-			return '/nav6.png'
-		case '5': // Принять результаты
-			return '/nav7.png'
-		case '6': // Обработать отказ
-			return '/nav5.png'
+const nav = ref(true)
+const closeNav = () => {
+	nav.value = false
+}
 
-		case '7': //  заявка отменена
-			return '/cancel.png'
-		case '8': //  заявка выполнена
-			return '/done.png'
-
-		default:
-			return '/empty.png'
-	}
+onMounted(() => {
+	gsap.from('.fuck', {
+		delay: 0.5,
+		x: -600,
+		opacity: 0,
+		ease: 'expo.out',
+	})
 })
 </script>
 
@@ -61,58 +48,26 @@ q-page
 			div роль:
 			q-select(v-model="role" dense fulled :options='options')
 
-		template(v-if='route.params.pag == 1')
+		template(v-if='route.params.pag == "1"')
 			FormTopEmulate(@setEtap='next')
 			FormEmulate1
 
-		// template(v-if='route.params.page == 1')
-		// 	FormTopEmulate(v-if='role == "Руководитель"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 2')
-		// 	FormTopEmulate(v-if='role == "Инициатор"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 3')
-		// 	FormTopEmulate(v-if='role == "Рассматривающий"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 4')
-		// 	FormTopEmulate(v-if='role == "Исполнитель"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 5')
-		// 	FormTopEmulate(v-if='role == "Инициатор"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 6')
-		// 	FormTopEmulate(v-if='role == "Инициатор"' @setEtap='next')
-		// 	FormEmulate1
-		//
-		// template(v-if='route.params.page == 7')
-		// 	h5 Заяка отменена
-		// 	br
-		// 	FormEmulate2
-		//
-		// template(v-if='route.params.page == 8')
-		// 	h5 Заявка выполнена
-		// 	br
-		// 	FormEmulate2
-
-	Teleport(to="body")
-		vue-draggable-resizable.fuck(:x="100" :y="-500" :w="350" :h="270" :active="false" :z="2000" :handles='["br"]' drag-handle='.bar')
-			q-card
-				.bar Навигация
-				q-img(:src='calcImg')
+	Teleport(to="body" v-if='nav')
+		vue-draggable-resizable.fuck(:x="100" :y="-300" :w="250" :h="150" :active="false" :z="2000" :handles='["br"]' drag-handle='.bar')
+			q-card(flat)
+				.bar
+					div Навигация
+					q-icon(name="mdi-close" @click='closeNav')
+				q-img(src='/empty.png')
 </template>
 
 <style scoped lang="scss">
 .preview {
 	width: 600px;
-	// background: #fff;
 	margin: 1rem auto;
 	height: 400px;
 }
+
 .bar {
 	height: 26px;
 	background: #dedede;
@@ -120,18 +75,36 @@ q-page
 	font-size: 0.9rem;
 	line-height: 26px;
 }
+
 .fuck {
 	border: 1px solid #aaa;
 }
+
 .tool {
 	width: 100%;
 	display: flex;
 	align-items: center;
 	justify-content: center;
 }
+
 .center {
 	display: flex;
 	gap: 1rem;
 	align-items: center;
+}
+
+.bar {
+	height: 26px;
+	background: #dedede;
+	padding: 0 6px;
+	font-size: 0.9rem;
+	line-height: 26px;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	.q-icon {
+		cursor: pointer;
+	}
 }
 </style>
